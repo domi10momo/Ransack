1448d9253c0b178dd17492bb32add8e9b1c9431b
hisadomi <hb.kmaia93@icloud.com>
2020-08-16 14:57:38 +0900

    seedにてデータ投入、UserSearchモデル作成

---
+++ app/models/user_search.rb
@@ -0,0 +1,8 @@
+class UserSearch
+  include ActiveModel::Module
+  attr_accessor :search_name, :search_age
+
+  def execute
+    User.ransack(name_eq: @search_name, age_eq: @search_age).result
+  end
+end
